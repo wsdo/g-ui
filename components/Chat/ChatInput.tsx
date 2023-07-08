f9ce4c8940f25e437b549a18f58667d4b72653cb
stark <wsd312@126.com>
2023-07-08 11:40:17 +0800

    init

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -379,7 +379,7 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://github.com/wsdo/g-ui"
           target="_blank"
           rel="noreferrer"
           className="underline"
